3653c7b6ff1ce5f901e0c31eddf4efb95c46d8e2
NikhilTeja <nikhil.teja@muthoot.com>
2024-12-12 19:37:43 +0530

    :tada: initial commit

---
+++ src/main/java/com/demo/flowcharts/FlowChartsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.flowcharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowChartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowChartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/flowcharts/FlowChartsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.flowcharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowChartsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
