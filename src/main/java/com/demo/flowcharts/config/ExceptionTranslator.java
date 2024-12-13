0a38b4accab16bbfa11e343c397742679b4d5d8d
Nikhil Teja G <nikhiltejagudla@gmail.com>
2024-12-13 11:18:03 +0530

    :art: refactor error response in exception translator

--- src/main/java/com/demo/flowcharts/config/ExceptionTranslator.java
+++ src/main/java/com/demo/flowcharts/config/ExceptionTranslator.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Map;
+import static com.demo.flowcharts.mapper.FlowChartMapper.body;
 
 @ControllerAdvice
 public class ExceptionTranslator extends ResponseEntityExceptionHandler {
 
+   private static final String ERROR_STATUS = "error";
+
    @ExceptionHandler(value = DataIntegrityViolationException.class)
    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
       return ResponseEntity.status(400)
-              .body(Map.of(
-                      "status", "error",
-                      "message", "Duplicate data found please check the chart and input"));
+              .body(body(ERROR_STATUS, "Duplicate data found please check the chart and input"));
    }
 
    @ExceptionHandler(value = FlowChartException.class)
    public ResponseEntity<Object> handleFlowChartException(FlowChartException ex) {
-      return ResponseEntity.status(ex.getStatus()).body(Map.of("status", "error", "message", ex.getMessage()));
+      return ResponseEntity.status(ex.getStatus()).body(body( ERROR_STATUS, ex.getMessage()));
    }
 
    @ExceptionHandler(value = RuntimeException.class)
    public ResponseEntity<Object> handleRunTimeException(RuntimeException ex) {
-      return ResponseEntity.status(400).body(Map.of("status", "error", "message", ex.getMessage()));
+      return ResponseEntity.status(400).body(body(ERROR_STATUS, ex.getMessage()));
    }
 }
